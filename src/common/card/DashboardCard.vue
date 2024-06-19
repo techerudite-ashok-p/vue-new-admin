4c0d2f9eb6ed11b019a60ffb020244e0d2d2c688
techerudite-ashok-p <ashok@techerudite.com>
2024-06-19 18:00:22 +0530

    change

---
+++ src/common/card/DashboardCard.vue
@@ -0,0 +1,72 @@
+<script setup>
+import { VBtn } from 'vuetify/lib/components/VBtn/index.mjs';
+import { VBreadcrumbsDivider } from 'vuetify/lib/components/index.mjs';
+
+const props = defineProps({
+  icon: String,
+  color: String,
+  title: String,
+  value: Number,
+  isHover: Boolean,
+});
+</script>
+
+<template>
+  <div>
+    <VCard class="logistics-card-statistics cursor-pointer" :style="isHover
+      ? `border-block-end-color: rgb(var(--v-theme-${color}))`
+      : `border-block-end-color: rgba(var(--v-theme-${color}),0.38)`
+      " @mouseenter="isHover = true" @mouseleave="isHover = false">
+      <VCardText>
+        <div class="d-flex align-center gap-x-4 mb-1">
+          <VAvatar variant="tonal" :color="color" rounded>
+            <VIcon :icon="icon" size="28" />
+          </VAvatar>
+          <h4 class="text-h4">
+            {{ value }}
+          </h4>
+        </div>
+        <div class="text-body-1 mb-1">
+          {{ title }}
+        </div>
+        <VDivider />
+        <div class="d-flex gap-x-2 align-center">
+          <VBtn size="40" variant="plain">
+            View
+          </VBtn>
+        </div>
+      </VCardText>
+    </VCard>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+@use "@core/scss/base/mixins" as mixins;
+
+.logistics-card-statistics {
+  border-block-end-style: solid;
+  border-block-end-width: 2px;
+  height: 100%;
+
+  &:hover {
+    border-block-end-width: 3px;
+    margin-block-end: -1px;
+
+    @include mixins.elevation(8);
+
+    transition: all 0.1s ease-out;
+  }
+}
+
+.skin--bordered {
+  .logistics-card-statistics {
+    border-block-end-width: 2px;
+
+    &:hover {
+      border-block-end-width: 3px;
+      margin-block-end: -2px;
+      transition: all 0.1s ease-out;
+    }
+  }
+}
+</style>
--- src/pages/dashboard/index.vue
+++ src/pages/dashboard/index.vue
@@ -2,44 +2,175 @@
 import { UserRegisterAction } from "../../components/graphql/mutation/userRegister";
 import { GetClinicListAction } from "../../components/graphql/query/getClinicList";
 import { useGetClinicList } from "../../components/pinia/getClinicList";
+import DashboardCard from "../../common/card/DashboardCard.vue"
 
 const clinicsStore = useGetClinicList();
-console.log("clinicsStoreclinicsStoreclinicsStore",clinicsStore?.page)
-const { initGetClinicList } =
-  GetClinicListAction();
+console.log("clinicsStoreclinicsStoreclinicsStore", clinicsStore?.page);
+const { initGetClinicList } = GetClinicListAction();
 
-const {initUserRegister,loading, error, data} =UserRegisterAction()  
+const { initUserRegister, loading, error, data } = UserRegisterAction();
 const fetchClinics = () => {
   console.log(
-    "initGetClinicListinitGetClinicListinitGetClinicListinitGetClinicList",clinicsStore?.page
+    "initGetClinicListinitGetClinicListinitGetClinicListinitGetClinicList",
+    clinicsStore?.page
   );
-   initGetClinicList({
-    page:(clinicsStore.page+1),
+  initGetClinicList({
+    page: clinicsStore.page + 1,
     limit: 10,
     search: "",
     isFeatured: false,
   });
   initUserRegister({
-  email: "pas285@yopmail.com",
-  password: "Test@12345",
-  fname: "Test",
-  lname: "Test",
-  number: "123465487990",
-  confirmPassword: "Test@12345"
-})
+    email: "pas285@yopmail.com",
+    password: "Test@12345",
+    fname: "Test",
+    lname: "Test",
+    number: "123465487990",
+    confirmPassword: "Test@12345",
+  });
 };
+
+const DashboardData = [
+  {
+    title: "Users",
+    value: 6002,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card",
+  },
+  {
+    title: "Phorest Users", value: 44, color: "primary", isHover: false, icon: "tabler-credit-card",
+  },
+  {
+    title: "Clinics", value: 2370, color: "primary", isHover: false, icon: "tabler-credit-card",
+  },
+  { title: "Client Forms", value: 8724, color: "primary", isHover: false, icon: "tabler-credit-card" },
+  { title: "Consent Forms", value: 12496, color: "primary", isHover: false, icon: "tabler-credit-card" },
+  { title: "Vendors", value: 227, color: "primary", isHover: false, icon: "tabler-credit-card" },
+  { title: "Products", value: 951, color: "primary", isHover: false, icon: "tabler-credit-card" },
+  { title: "Orders Count", value: 2225, color: "primary", isHover: false, icon: "tabler-credit-card" },
+  {
+    title: "Orders Commissions",
+    value: "£ 228499.76",
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Deposit Commissions",
+    value: "£ 8190.68",
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Prescription Fees",
+    value: "£ 18869.71",
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Prescription Orders",
+    value: 1625,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Client Subscription Fees",
+    value: "£ 25748807.43",
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Shop Unpaid Total Amount",
+    value: 92,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Unprocessed Prescription Count",
+    value: 92,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Unprocessed Prescription Total",
+    value: "£ 41086.77",
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Completed Clinic Profiles",
+    value: 129,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Partial Clinic Profiles",
+    value: 125,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Incomplete Clinic Profiles",
+    value: 2115,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Total completed websites",
+    value: 354,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  { title: "In progress websites", value: 964, color: "primary" },
+  {
+    title: "Thank You Page Orders Count",
+    value: 16,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+  {
+    title: "Thank You Page Prescription Orders",
+    value: 7,
+    color: "primary",
+    isHover: false,
+    icon: "tabler-credit-card"
+  },
+];
+
+console.log("DashboardDataDashboardData", DashboardData);
 </script>
 
 <template>
   <div>
-    <VCard title="Create Awesome 🙌">
-      <VCardText>This is your second page.fdgfdg</VCardText>
-      <VBtn @click="fetchClinics" color="primary">Fetch Clinics</VBtn>
-      <VCardText>
-        Chocolate sesame snaps pie carrot cake pastry pie lollipop muffin.
-        Carrot cake dragée chupa chups jujubes. Macaroon liquorice cookie wafer
-        tart marzipan bonbon. Gingerbread jelly-o dragée chocolate.
-      </VCardText>
-    </VCard>
+    <VRow class="match-height">
+      <VCol cols="12" md="3" sm="6" v-for="(item, index) in DashboardData" :key="index">
+        <DashboardCard :icon="item.icon" :color="item.color" :title="item.title" :value="item.value"
+          :isHover="item.isHover" />
+      </VCol>
+
+    </VRow>
+
+    <VBtn @click="fetchClinics" color="primary">Fetch Clinics</VBtn>
+
   </div>
 </template>
+
+
+<style scoped>
+.match-height {
+  height: 100%;
+}
+</style>
